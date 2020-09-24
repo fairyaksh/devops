db5b06857f2970ea194738da23dbff0595429f98
Akshaya <akshayasatheesh@hotmail.co.uk>
2020-09-24 11:36:26 +0100

    commit message

--- test.js
+++ test.js
@@ -3,21 +3,21 @@ $("#speedPercent").on("input", (event) => {
 });
 
 $("#grid").on("click", (event) => {
-  if (event.target && event.target .matches(" button.banana")) {
-    const points =  parseInt(event.target.dataset.points, 10);
-       state =  { ...state, score: state.score + points };
-    setScoreInnerHTML(state) ;
+  if (event.target && event.target.matches(" button.banana")) {
+    const points = parseInt(event.target.dataset.points, 10);
+    state = { ...state, score: state.score + points };
+    setScoreInnerHTML(state);
 
     const span = event.target.querySelector("span");
-      span.classList.add(" exit-animation");
-    span.on("animationend", () => { 
-         event.target.parentNode.removeChild(event.target);
-      });
+    span.classList.add(" exit-animation");
+    span.on("animationend", () => {
+      event.target.parentNode.removeChild(event.target);
+    });
   }
 });
 
-const   mutationObserver = observer(state);
-  mutationObserver.observe($("#grid"), {
+const mutationObserver = observer(state);
+mutationObserver.observe($("#grid"), {
   attributes: false,
   childList: true,
   subtree: true,
