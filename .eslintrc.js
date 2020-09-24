78e7557ef7955c0e2b2da188197c3427d446a100
Akshaya <akshayasatheesh@hotmail.co.uk>
2020-09-24 11:12:01 +0100

    errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    env: {
-      node: true,
-    },
-    extends: ["airbnb-base", "plugin:prettier/recommended"],
-    parserOptions: {
-      ecmaVersion: 2018,
-    },
-  };
+  env: {
+    node: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+};
